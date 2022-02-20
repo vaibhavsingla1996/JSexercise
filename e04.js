c8ca53c58a73809c46666639d123cf8002ce6c6e
vaibhavsingla1996 <vaibhavsingla1996@gmail.com>
2022-02-20 15:09:24 +0530

    completed js exercise 4

---
+++ e04.js
@@ -0,0 +1,45 @@
+
+// String
+let str = "Vaibhav singla"
+console.log(str)
+console.log("Data type is "+ (typeof str))
+
+// Int
+let age = 25
+console.log(age)
+console.log("Data type is "+ (typeof age))
+
+// Boolean 
+let isWorkingProfessional = true
+console.log(isWorkingProfessional)
+console.log("Data type is "+ (typeof isWorkingProfessional))
+
+
+isWorkingProfessional = "Jai Mata di"
+console.log(isWorkingProfessional)
+console.log("Updated datatype type is "+ (typeof isWorkingProfessional))
+
+// Null 
+let isMarried = null
+console.log(isMarried)
+console.log("Data type is "+ (typeof isMarried))
+
+// updated value from null to boolean
+isMarried = false
+console.log(isMarried)
+console.log("Updated datatype is "+ (typeof isMarried))
+
+// undefiend
+let companyName
+console.log(companyName)
+console.log("Data type is "+ (typeof companyName))
+
+// another way to define undefined
+let collegeName = undefined
+console.log(collegeName)
+console.log("Data type is "+ (typeof collegeName))
+
+// updated value in undefined
+collegeName = "DCRUST"
+console.log(collegeName)
+console.log("Updated datatype is "+ (typeof collegeName))
